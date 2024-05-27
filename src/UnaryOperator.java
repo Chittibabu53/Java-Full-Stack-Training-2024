c787cf0d8de5e4859a98905e6136a30b1842ee21
Chittibabu <chittiakhil53@gmail.com>
2024-05-27 21:46:50 +0530

    palindrome

---
+++ src/UnaryOperator.java
@@ -0,0 +1,12 @@
+public class UnaryOperator {
+
+    public static void main(String [] args){
+        int a=10,b=6;
+
+        System.out.println(a++);
+        System.out.println(++a);
+        System.out.println(b--);
+        System.out.println(b++);
+
+    }
+}
