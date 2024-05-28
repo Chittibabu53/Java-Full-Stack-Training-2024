b3a246c889e0bc8af342d2029cf67c4625be7f4e
Chittibabu <chittiakhil53@gmail.com>
2024-05-28 10:21:34 +0530

    palindrome

---
+++ src/AssignmentOperator.java
@@ -0,0 +1,14 @@
+public class AssignmentOperator {
+
+    public static void main (String [] args){
+        int a=10,b=5;
+        System.out.println(a+=3);
+        System.out.println(a-=3);
+        System.out.println(a*=4);
+        System.out.println(a/=3);
+        System.out.println(a%=3);
+
+
+
+    }
+}
