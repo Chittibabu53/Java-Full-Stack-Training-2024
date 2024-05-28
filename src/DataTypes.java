d8bcf6a53680908823068e105bc5cc16a860e4e0
Chittibabu <chittiakhil53@gmail.com>
2024-05-28 11:45:32 +0530

    operators

---
+++ src/DataTypes.java
@@ -0,0 +1,28 @@
+public class DataTypes {
+    public static void main (String [] args){
+
+        char a='m';
+        int i=10;
+        byte b= 8;
+        short x=10;
+        long y=20;
+
+        boolean like=false;
+
+        float h=3.2f;
+
+        double d=2.2222;
+
+        System.out.println(a);
+        System.out.println(i);
+        System.out.println(b);
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(like);
+        System.out.println(h);
+        System.out.println(d);
+
+
+
+    }
+}
