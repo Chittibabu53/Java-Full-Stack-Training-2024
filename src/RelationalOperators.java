64d82b5a5e9f3e206bb85938bf17562387d70584
Chittibabu <chittiakhil53@gmail.com>
2024-05-28 10:59:52 +0530

    operators

---
+++ src/RelationalOperators.java
@@ -0,0 +1,16 @@
+public class RelationalOperators {
+
+    public static void main (String [] args){
+
+        int a=10, b=5,c=3;
+
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+        System.out.println(a==c);
+        System.out.println(a!=c);
+
+
+    }
+}
