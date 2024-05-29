5cfd51d5572e82e0daf53a6e7c4b40d2c2072bb3
Chittibabu <chittiakhil53@gmail.com>
2024-05-29 12:50:28 +0530

    operators

--- TypeCastingPrograms/Typecasting.java
+++ TypeCastingPrograms/Typecasting.java
@@ -3,10 +3,33 @@ public class Typecasting {
 
         int mtInt=9;
 
+        byte bh=4;
+
+        short sh=3;
+
+        long lj=2;
+
+        float f=1.2333f;
+
+        double df=1.3333;
+
         double myDouble=mtInt;
 
+        float myFloat=mtInt;
+
+        double mylong=f;
+
+
+
         System.out.println(mtInt);
+
         System.out.println(myDouble);
+
+        System.out.println(myFloat);
+
+
+
+
 
     }
 }
