5fc7db239bf80ded2a7b441c4ee30a5ebd83c8c8
Chittibabu <chittiakhil53@gmail.com>
2024-05-29 10:03:21 +0530

    operators

--- src/AssignmentOperator.java
+++ src/AssignmentOperator.java
@@ -2,11 +2,11 @@ public class AssignmentOperator {
 
     public static void main (String [] args){
         int a=10,b=5;
-        System.out.println(a+=3);
-        System.out.println(a-=3);
-        System.out.println(a*=4);
-        System.out.println(a/=3);
-        System.out.println(a%=3);
+        System.out.println((a)+=3);
+        System.out.println((a)-=3);
+        System.out.println((a)*=4);
+        System.out.println((a)/=3);
+        System.out.println((a)%=3);
 
 
 
