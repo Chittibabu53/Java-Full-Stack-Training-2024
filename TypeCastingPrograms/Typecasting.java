74e6d4eb3f56b17702694db3941b07317ed5a1dd
Chittibabu <chittiakhil53@gmail.com>
2024-05-29 12:01:28 +0530

    operators

---
+++ TypeCastingPrograms/Typecasting.java
@@ -0,0 +1,12 @@
+public class Typecasting {
+    public static void main (String[] args){
+
+        int mtInt=9;
+
+        double myDouble=mtInt;
+
+        System.out.println(mtInt);
+        System.out.println(myDouble);
+
+    }
+}
