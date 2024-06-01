a3e763f7ecac1466c22738c8a7fc49a0d1c23dca
Chittibabu <chittiakhil53@gmail.com>
2024-06-01 14:52:13 +0530

    week 1

---
+++ Week 1/CountCharacters.java
@@ -0,0 +1,17 @@
+public class CountCharacters {
+
+    public static void main (String [] args){
+
+        String name="mahesh";
+
+        char [] array=name.toCharArray();
+
+        int count =array.length;
+
+        System.out.println(count);
+
+
+
+
+    }
+}
