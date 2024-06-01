15cc56a6ce5cd170f06c9fbf4748926843b7d31a
Chittibabu <chittiakhil53@gmail.com>
2024-06-01 16:42:11 +0530

    week 1

---
+++ WrapperClasses/InstanceOf.java
@@ -0,0 +1,11 @@
+public class InstanceOf {
+    public static void main (String [] args){
+        int [] array=new int[2];
+
+       boolean result= array instanceof int[];
+       System.out.println(result);
+
+
+
+    }
+}
