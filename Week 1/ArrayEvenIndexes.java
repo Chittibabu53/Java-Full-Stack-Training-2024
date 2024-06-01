417a37f5656721302381baa337950acbd1b0c384
Chittibabu <chittiakhil53@gmail.com>
2024-06-01 13:29:02 +0530

    week 1

--- Week 1/ArrayEvenIndexes.java
+++ Week 1/ArrayEvenIndexes.java
@@ -1,3 +1,16 @@
+public class ArrayEvenIndexes{
 
+    public static void main (String[] args){
 
+        int [] array={1,2,3,4,5,6,7};
+
+        for(int i=0;i< array.length;i++){
+            if(i%2==0){
+                System.out.println(array[i]);
+            }
+
+        }
+
+    }
+}
 
