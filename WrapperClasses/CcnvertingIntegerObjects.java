6a11b383271c08f48136aad7fa0818efc2eb8878
Chittibabu <chittiakhil53@gmail.com>
2024-06-02 18:58:54 +0530

    week 1

---
+++ WrapperClasses/CcnvertingIntegerObjects.java
@@ -0,0 +1,43 @@
+public class CcnvertingIntegerObjects {
+    public static void  main(String[] args){
+
+        Integer num1=Integer.valueOf(222);
+
+        int num2=num1;
+
+
+        Double doubleValue=Double.valueOf(2);
+
+        double d1=doubleValue;
+
+        System.out.println(d1);
+
+
+        Float floatValue =Float.valueOf(3);
+
+        float f1=floatValue;
+
+
+        Long longValue =Long.valueOf(8);
+
+        long l1=longValue;
+
+
+        Short shortvalue=Short.valueOf((short) 20);
+
+        short s1=shortvalue;
+
+        System.out.println(s1);
+
+
+        Byte byteValue =Byte.valueOf((byte)11);
+
+        byte b1=byteValue;
+
+
+
+
+
+
+    }
+}
--- WrapperClasses/InstanceOf.java
+++ WrapperClasses/InstanceOf.java
@@ -6,6 +6,5 @@ public class InstanceOf {
        System.out.println(result);
 
 
-
     }
 }
