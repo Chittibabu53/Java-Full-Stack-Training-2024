4db92aeaae5df628ed147259ab35b8f7f227b7bf
Chittibabu <chittiakhil53@gmail.com>
2024-06-02 21:41:13 +0530

    week 1

---
+++ Week 1/SecondLargestElement.java
@@ -0,0 +1,18 @@
+public class SecondLargestElement {
+    public static void main(String[] args) {
+        int[] numbers = {3, 5, 7, 2, 8, 10, 6, 4};
+        int firstMax = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+
+        for (int num : numbers) {
+            if (num > firstMax) {
+                secondMax = firstMax;
+                firstMax = num;
+            } else if (num > secondMax && num != firstMax) {
+                secondMax = num;
+            }
+        }
+
+        System.out.println("The second largest element is " + secondMax);
+    }
+}
