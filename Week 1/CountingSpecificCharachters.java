341674f2baeab79deadb83efdfcd62a159dfef82
Chittibabu <chittiakhil53@gmail.com>
2024-06-02 20:55:45 +0530

    week 1

---
+++ Week 1/CountingSpecificCharachters.java
@@ -0,0 +1,21 @@
+public class CountingSpecificCharachters {
+
+    public static void  main (String [] args){
+                String text = "hello world";
+                char target = 'l';
+                int count = 0;
+
+                System.out.println(text.length());
+
+                for (int i = 0; i < text.length(); i++) {
+                    if (text.charAt(i) == target) {
+                        count++;
+                    }
+                }
+
+                System.out.println("The character '" + target + "' appears " + count + " times in the string.");
+            }
+        }
+
+
+
