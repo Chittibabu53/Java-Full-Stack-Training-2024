ffaf6871fc10329e60f8fc5d0c08a78c42541c0b
Chittibabu <chittiakhil53@gmail.com>
2024-06-02 20:09:26 +0530

    week 1

---
+++ Operators/Uses.java
@@ -0,0 +1,15 @@
+public class Uses {
+
+    public static void main(String [] args)
+
+    {
+      int  documentno =0b00000000;
+      documentno |=(1<<3);  //setting the bit 4
+
+        documentno &= ~(1<<3); //clearing the bit
+
+
+
+
+    }
+}
