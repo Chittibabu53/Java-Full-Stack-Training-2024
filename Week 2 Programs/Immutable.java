1a68ea73d808e79c126588c086470dc6d2aa8a86
Chittibabu <chittiakhil53@gmail.com>
2024-06-03 16:35:42 +0530

    week 1

---
+++ Week 2 Programs/Immutable.java
@@ -0,0 +1,25 @@
+public class Immutable {
+        public static void main(String[] args) {
+
+            Integer num=10;
+            System.out.println(System.identityHashCode(num));
+
+
+            Integer num1=10;
+            System.out.println(System.identityHashCode(num1));
+
+
+
+
+
+
+
+
+        }
+    }
+
+
+
+
+
+
---
+++ Week 2 Programs/InstanceUsing.java
@@ -0,0 +1,18 @@
+
+class Dog{
+
+}
+class Animal extends Dog{
+    
+}
+class Rabbit extends  Animal{
+
+}
+
+
+public class InstanceUsing {
+
+    public static void main (String[] args){
+
+    }
+}
