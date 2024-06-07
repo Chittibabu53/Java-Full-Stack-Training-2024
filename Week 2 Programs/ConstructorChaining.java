0dae465eb2a56a14332e5fae9ee782e2caac8442
Chittibabu <chittiakhil53@gmail.com>
2024-06-07 16:54:49 +0530

    week 2

---
+++ Week 2 Programs/ConstructorChaining.java
@@ -0,0 +1,23 @@
+public class ConstructorChaining {
+
+    int x=55;
+    ConstructorChaining(int x , int y){
+        this(9);
+    }
+    ConstructorChaining(int l){
+       this(9,99,99);
+    }
+
+    ConstructorChaining(int m,int k,int l){
+
+    }
+    void method(int m){
+
+    }
+
+
+    public static void main(String [] args){
+
+
+    }
+}
