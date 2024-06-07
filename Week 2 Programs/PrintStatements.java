45449f56f8a0dcfaa3767114b79771f383fa4945
Chittibabu <chittiakhil53@gmail.com>
2024-06-07 12:23:34 +0530

    week 2

---
+++ Week 2 Programs/PrintStatements.java
@@ -0,0 +1,35 @@
+public class PrintStatements {
+
+
+    public static void main (String[] args){
+
+                // Basic print statements
+                System.out.print("Hello, ");
+                System.out.print("World!");
+                System.out.println();
+
+                // Print with new lines
+                System.out.println("Hello, World!");
+                System.out.println("Line 1\nLine 2\nLine 3");
+
+                // Print with tab spaces
+                System.out.println("Column1\tColumn2\tColumn3");
+
+                // Print with multiple spaces
+                System.out.println("This is     spaced text.");
+
+                // Combining different formatting
+                System.out.println("Name:\tJohn Doe\nAge:\t30\nCity:\tNew York");
+
+                System.out.printf("Name: %s, Age: %d", "John", 25);
+
+                System.out.println();
+                // Output: Name: John, Age: 25
+
+               int x=9,y=8;
+               System.out.println(x+"\t"+y);
+
+    }
+        }
+
+
