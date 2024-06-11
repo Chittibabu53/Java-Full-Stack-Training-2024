2d04fb4afa797185d6936ba1590d1c6c9691b9a4
chitti <chittiakhil53@gmail.com>
2024-06-11 16:17:15 +0530

    week 2

---
+++ Week3/RunTimePolymorphism.java
@@ -0,0 +1,35 @@
+class Parent {
+
+    void run(){
+        System.out.println("i am parent");
+    }
+
+}
+class Child extends Parent{
+
+    void run(){
+        System.out.println("i am class");
+    }
+
+
+}
+
+
+
+
+public class RunTimePolymorphism {
+
+    public static void main(String[]args){
+
+        Parent parent;
+        Parent child;
+
+         parent=new Parent();
+         child=new Child();
+
+        parent.run();
+        child.run();
+
+
+    }
+}
