7cef55d452f242d5eccf4e4289f573284da8e176
chitti <chittiakhil53@gmail.com>
2024-06-12 12:40:54 +0530

    week 3

---
+++ Practice/Matrixr.java
@@ -0,0 +1,48 @@
+public class Matrixr {
+
+    public static void main(String[] args){
+
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        //transpose matrix
+        for(int i=0;i< matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                 int temp=matrix[i][j];
+
+                 matrix[i][j]=matrix[j][i];
+                 matrix[j][i]=temp;
+
+            }
+        }
+
+
+        //reverse the matrix
+
+
+        for(int i=0;i< matrix.length;i++){
+           for(int j=0;j< matrix.length/2;j++){
+               int temp=matrix[i][j];
+
+               matrix[i][j]=matrix[i][matrix.length-1-j];
+
+               matrix[i][matrix.length-1-j]=temp;
+
+           }
+        }
+
+        for(int [] num:matrix){
+            for(int num1:num){
+                System.out.print(num1+" ");
+            }
+            System.out.println();
+        }
+
+
+
+
+    }
+}
