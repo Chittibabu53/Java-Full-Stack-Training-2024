608eae5dccebe4fca80eb5095e258b32de82f819
chitti <chittiakhil53@gmail.com>
2024-06-12 09:50:02 +0530

    week 3

--- Practice/ReversingAnArray.java
+++ Practice/ReversingAnArray.java
@@ -1,16 +1,34 @@
 public class ReversingAnArray {
-    public static void  main (String[] args){
+    public static void  main (String[] args) {
 
-        int [] array=new int[]{5,6,7,8,9,5,2,3,43};
+        int[] array = new int[]{5, 6, 7, 8, 9, 5, 2, 3, 43};
 
-        int last= array.length-1;
+        int i=0;
+        int last = array.length - 1;
 
-//        for (int i=last;i>= 0;i--){
+//        for ( i=last;i>= 0;i--){
 //
 //            System.out.print(" "+array[i]);
 //
 //        }
 
+
+        while(i<last){
+
+            int temp=array[i];
+            array[i]=array[last];
+
+            array[last]=temp;
+
+         i++;
+         last--;
+
+        }
+
+        for(int num:array){
+
+            System.out.println(num);
+        }
 
     }
 }
