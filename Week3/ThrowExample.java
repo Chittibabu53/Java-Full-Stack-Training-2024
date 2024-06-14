317909aeab3a633d594023b76f30f646dbf3ce4e
chitti <chittiakhil53@gmail.com>
2024-06-14 11:58:28 +0530

    week 3

--- Week3/ThrowExample.java
+++ Week3/ThrowExample.java
@@ -1,13 +1,15 @@
+import java.io.IOException;
+
 public class ThrowExample {
     public static void main(String[] args) {
         try {
-            checkAge(15);
-        } catch (ArithmeticException e) {
+            checkAge(21);
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public static void checkAge(int age) {
+    public static void checkAge(int age) throws  ArithmeticException {
         if (age < 18) {
             throw new ArithmeticException("Access denied - You must be at least 18 years old.");
         } else {
--- Week3/ThrowsExample.java
+++ Week3/ThrowsExample.java
@@ -1,9 +1,35 @@
+import java.io.IOException;
+
 public class ThrowsExample {
 
-    public static void main(String[] args){
 
 
+        public static void main(String[] args) {
+            try{
+                throwException();
+            }
+            catch(Exception e){
+
+            }
+
+        }
+
+        public static void throwException() throws Exception {
+
+
+            int a=10/0;
+
+            throw  new IOException();
+//            try {
+//                int a=0/10;
+//            } catch (Exception t) {
+//                System.out.println("Caught a Throwable: " + t.getMessage());
+//                t.printStackTrace();
+//            }
+        }
     }
 
 
-}
+
+
+
