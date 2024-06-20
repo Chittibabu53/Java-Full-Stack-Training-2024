73956e9c52dc2c2618015b940c2e9e067204ca6b
chitti <chittiakhil53@gmail.com>
2024-06-20 16:06:40 +0530

    week 4

---
+++ Week4/QueueExample.java
@@ -0,0 +1,29 @@
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedDeque;
+
+public class QueueExample {
+
+    public static void main(String[] args){
+
+        Queue<Integer> sample=new ConcurrentLinkedDeque<>();
+
+
+
+        sample.add(1);
+        sample.add(2);
+        sample.add(3);
+
+//        sample.poll();
+
+//        System.out.println(sample.peek());
+
+        for(int numjj:sample){
+            System.out.println(numjj);
+
+            sample.poll();
+        }
+
+    }
+}
