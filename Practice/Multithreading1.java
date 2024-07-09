11e0dfffbcf00e5eb05d7b3ee04210597a8d268b
chitti <chittiakhil53@gmail.com>
2024-07-09 11:50:23 +0530

    multithreading

--- Practice/Multithreading1.java
+++ Practice/Multithreading1.java
@@ -1,17 +1,59 @@
-public  class Multithreading1  {
+class Zoom{
+ private final Object lock=new Object();
 
+ private boolean condition=false;
 
-    public static void main(String[] args) throws Exception {
+ public void waitForCondition() throws InterruptedException{
 
-        try{
-            for(int i=0;i<10;i++){
-                Thread.sleep(1000);
-                System.out.println(i);
+     synchronized (lock){
+         while(!condition){
+             System.out.println("before wait");
+             lock.wait();
+         }
+     }
+ }
+ public void waitForNotification(){
+     synchronized (lock){
+         condition=true;
+         lock.notify();
+         System.out.println("after notify");
+     }
+ }
+
+}
+
+public class Multithreading1 extends Zoom {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Multithreading1 obj=new Multithreading1();
+
+        Thread waitingThread=new Thread(()->{
+           try{
+               System.out.println("thread is waiting");
+             obj.waitForCondition();
             }
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
+           catch(Exception e){
+             e.printStackTrace();
+            }
+        });
+
+        Thread notifyThread=new Thread(()->{
+            obj.waitForNotification();
+            System.out.println("notify finished");
+        });
+
+
+
+
+        waitingThread.start();
+        Thread.sleep(1000);
+        notifyThread.start();
+
+        waitingThread.join();
+        notifyThread.join();
+
+
 
     }
 }
