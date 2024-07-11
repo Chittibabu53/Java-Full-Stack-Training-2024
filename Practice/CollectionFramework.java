e1e8b89cb32bd3e7a870b6abd05edb64ee7b4f7b
chitti <chittiakhil53@gmail.com>
2024-07-11 23:18:16 +0530

    filters

--- Practice/CollectionFramework.java
+++ Practice/CollectionFramework.java
@@ -198,27 +198,31 @@ class CollectionFramework{
         localDates.forEach(System.out::println);
 
 
-        PriorityQueue<Integer>  queue=new PriorityQueue();
+        PriorityQueue<String>  queue=new PriorityQueue();
 
-//        queue.add(10);
-//        queue.add(20);
-//        queue.add(15);
-//        queue.add(9);
+        queue.add("A");
+        queue.add("ab");
+        queue.add("a");
+        queue.add("c");
+        queue.add("A");
 
-        for(int i=9;i>=0;i--){
-            queue.add(i);
-        }
 
-        // Printing the top element of PriorityQueue
-        System.out.println(queue.peek());
 
-        // Printing the top element and removing it
-        // from the PriorityQueue container
-        System.out.println(queue.poll());
 
-        // Printing the top element again
-        System.out.println(queue.peek());
+//        // Printing the top element of PriorityQueue
+//        System.out.println(queue.peek());
+//
+//        // Printing the top element and removing it
+//        // from the PriorityQueue container
+////        System.out.println(queue.poll());
+//
+//        // Printing the top element again
+//        System.out.println(queue.peek());
+//
+//
+//        System.out.println(queue);
 
+        queue.remove("A");
 
         System.out.println(queue);
 
---
+++ Practice/Recursion.java
@@ -0,0 +1,41 @@
+public class Recursion extends Thread {
+    @Override
+    public void run() {
+        System.out.println((recurse(2,3)));
+    }
+
+    static int recurse(int base, int power){
+
+
+        if(base==power){
+            base*=base;
+        }
+        if(base==power*power){
+            base*=base/power;
+
+            return base;
+
+        }
+        power--;
+
+
+        return recurse(base,power);
+    }
+
+    public static void main(String[] args) {
+
+
+       Recursion thread1=new Recursion();
+       Recursion thread2=new Recursion();
+
+       thread1.start();
+       thread2.start();
+
+
+
+
+
+
+
+    }
+}
