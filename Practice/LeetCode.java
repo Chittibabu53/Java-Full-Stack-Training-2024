1e57ab668a87fc4d2c91be22cbad3fb2cbdaa133
chitti <chittiakhil53@gmail.com>
2024-07-22 12:12:59 +0530

    CollectionFramework

---
+++ Practice/LeetCode.java
@@ -0,0 +1,21 @@
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class LeetCode {
+
+
+    public static void main(String[] args) {
+
+      String[] names={"Mary","John","Emma"};
+      int [] heights={180,165,170};
+
+        TreeMap<Integer,String> treeMap=new TreeMap<>(Comparator.reverseOrder());
+
+        for (int i = 0; i < names.length; i++) {
+            treeMap.put(heights[i],names[i]);
+        }
+
+        System.out.println(treeMap.values());
+    }
+}
--- Practice/LinkedMaps.java
+++ Practice/LinkedMaps.java
@@ -5,24 +5,34 @@ public class LinkedMaps {
     public static void main(String[] args) {
 
         List<Integer> list=new ArrayList<>();
-        Collections.addAll(list,1,2,3,4,3,4,5,5,5);
+        Collections.addAll(list,1,2,3,4,3,4,5);
         Map<Integer,Integer> map=new HashMap<>();
 
         list.forEach((x)->
         {
-            if(!map.containsKey(x)){
-                map.put(x, 1);
-            }
-            else {
-                map.put(x,map.get(x)+1);
-            }
+            map.compute(x, (key, oldValue) -> oldValue == null ? 1 : oldValue + 1);
         });
 
         map.forEach((x,y)-> {
-            if(y==2){
-                System.out.println(x);
-            }
+            if (y==2) System.out.println(x);
         });
 
     }
 }
+
+
+
+//            map.compute(x, (key, oldValue) -> {
+//                if (oldValue == null) {
+//                    return 1; // Initialize to 1 if key is not present
+//                } else {
+//                    return oldValue + 1; // Increment the value if key is already present
+//                }
+//            });
+
+//            if(!map.containsKey(x)){
+//                map.put(x, 1);
+//            }
+//            else {
+//                map.put(x,map.get(x)+1);
+//            }
